075ca4662daa93c26de0be80641063f339b97a27
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-02-02 14:00:20 +1000

    Added http server / logrus

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/mvanbrummen/go-repl
+
+require github.com/sirupsen/logrus v1.3.0
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./frontend"))
+
+	http.Handle("/", fs)
+
+	log.Println("Listening on :3000...")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
