8ec49fe569b2fdf7cf7ed903cb65f9d03a71a1b4
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-02-02 14:36:43 +1000

    Added react ace split pane

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import brace from 'brace';
+import 'brace/mode/golang';
+import 'brace/theme/github';
+import { split as SplitEditor } from 'react-ace';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="editor">
+        <SplitEditor
+          theme="github"
+          mode="golang"
+          name="replEditor"
+          editorProps={{ $blockScrolling: true }}
+          splits={2}
+          orientation="besides"
+          width="100%"
+          height="600px"
+        />
       </div>
     );
   }
 }
-
-export default App;
