e85d6cd59f02a9b472161e9466385af4215a34da
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-02-02 14:55:05 +1000

    Changed theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import brace from 'brace';
 import 'brace/mode/golang';
-import 'brace/theme/github';
+import 'brace/theme/solarized_dark';
 import { split as SplitEditor } from 'react-ace';
+import { FaPlay } from 'react-icons/fa';
 
 export default class App extends Component {
   render() {
     return (
       <div className="editor">
+        <h1>gorepl</h1>
+        <button id="playBtn"><FaPlay /></button>
+
         <SplitEditor
-          theme="github"
+          theme="solarized_dark"
           mode="golang"
           name="replEditor"
           editorProps={{ $blockScrolling: true }}
